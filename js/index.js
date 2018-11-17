62a5e6f14fe435988e98590e4c8d60696c5e5614
Agostinabufano <agostinabufano@gmail.com>
2018-11-17 10:30:38 -0300

    cards size

--- js/index.js
+++ js/index.js
@@ -13,25 +13,13 @@ var movements;
 
 $(".facil").on("click", function () {
     movements = 18;
-    console.log(movements)
 })
 $(".intermedio").on("click", function () {
     movements = 12;
-    console.log(movements)
 })
 $(".experto").on("click", function () {
     movements = 9;
-    console.log(movements)
 })
-
-// function game () {
-//     clicks;
-//     mmovimientos <= clicks
-//     if (click a una imag) {
-// click++
-//     }
-//     clcik = 0
-// }
 
 var arr = [];
 
@@ -69,10 +57,45 @@ var imgsLength = $('.cardsImg').length
 
 for (var i = 0; i < imgsLength; i++) {
   $('.cardsImg').children('img').eq(i).attr('data-img', arr[i].url)
+  $('.cardsImg').children('img').eq(i).attr('data-pos', i)
+  $('.cardsImg').eq(i).attr('id', arr[i].id)
 }
 
 $('.cardsImg').on('click', function() {
   var visible = $(this).children('img').attr('data-img')
-  console.log(visible)
   $(this).children('img').attr('src', visible)
 })
+
+var clicks = 2;
+var primerClick
+
+$(".cardsImg").on ("click", function () {
+    $(this).addClass("noPointer");
+    clicks = clicks - 1
+    var pos = $(this).children('img').attr('data-pos')
+    if (clicks == 1) {
+        primerClick = arr[pos]
+    } else {
+        console.log(arr[pos].url, primerClick.url)
+        if (arr[pos].url == primerClick.url) {
+            $(this).addClass("greyCard")
+            $("#" + primerClick.id).addClass("greyCard")
+            console.log('iguales')
+        } else {
+           $(this).children('img').removeAttr('data-img')
+            console.log('distintas')
+            clicks = 2
+        }
+    }  
+})
+
+// var paresEncontrados = 0; 
+
+// function game () {
+//     clicks;
+//     mmovimientos <= clicks
+//     if (click a una imag) {
+// click++
+//     }
+//     click = 0
+// }
